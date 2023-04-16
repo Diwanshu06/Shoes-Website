7d533930a919e07c375c3027defefeaa71e2e2f1
Diwanshu06 <89571816+Diwanshu06@users.noreply.github.com>
2023-04-16 23:51:50 +0530

    Delete ShoesSelling Website directory

--- ShoesSelling Website/mangola/includes/css_header.php
+++
@@ -1,5 +0,0 @@
-<head>
-	<title>JooteKhareedo | The home of shoes</title>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-</head>
